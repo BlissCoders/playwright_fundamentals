19407f65c5a4826cbbffb8c160769ffc850db4a2
Dheo Claveria <dheybenclave@gmail.com>
2026-03-03 01:52:37 +0800

    created TC8-13 in interaction_page and Added dynamic methods

--- src/pages/interactions_page.py
+++ src/pages/interactions_page.py
@@ -1,3 +1,6 @@
+import re
+from time import sleep
+
 from playwright.sync_api import Locator, Page, expect
 
 
@@ -22,7 +25,39 @@ class InteractionsPage:
     def lbl_text(self, text) -> Locator:
         return self.page.get_by_text(text)
 
+    def txt_autocomplete(self):
+        return self.page.locator("//input[@id='autocomplete']")
+
+    def txt_date_picker(self):
+        return self.page.locator("//input[@id='datepicker' and @class='hasDatepicker']")
+
+    def tab_selected(self, tabNum: str) -> Locator:
+        return self.page.locator(f'//li[@role="tab" and @aria-controls="tabs-{tabNum}"]')
+
+    def get_locator_by(self: Page, locator_type: str, value: str) -> Locator:
+        """
+        Returns a locator based on the type specified.
+        Types: 'alt_text', 'label', 'placeholder', 'role', 'test_id', 'text', 'title'
+        """
+        if locator_type == "alt_text":
+            return self.page.get_by_alt_text(value)
+        elif locator_type == "label":
+            return self.page.get_by_label(value)
+        elif locator_type == "placeholder":
+            return self.page.get_by_placeholder(value)
+        elif locator_type == "role":
+            return self.page.get_by_role(value)
+        elif locator_type == "test_id":
+            return self.page.get_by_test_id(value)
+        elif locator_type == "text":
+            return self.page.get_by_text(value)
+        elif locator_type == "title":
+            return self.page.get_by_title(value)
+        else:
+            return self.locator(value)
+
     # Commands
+
     @classmethod
     def open(cls, page, url: str = "https://blisscoders.pythonanywhere.com/") -> 'InteractionsPage':
         page.goto(url)
@@ -30,16 +65,51 @@ class InteractionsPage:
 
     def navigate_to(self, nav_text):
         print(f"Navigating to {nav_text} ")
-
         self.nav_menu_items().filter(has_text=nav_text).click()
 
     def drag_and_drop(self):
         print(f"Dragging element with selector 'btn_drag_me' to element with selector 'sec_drop_here'")
         self.btn_drag_me().drag_to(self.sec_drop_here())
 
+    def focus_element(self, locator: Locator):
+        print(f"Focus Element {locator}")
+        self.verify_element_visible(locator)
+        locator.focus()
+        expect(locator).to_be_focused()
+
+    def click_element(self, locator: Locator):
+        print(f"Click Element {locator}")
+        self.verify_element_visible(locator)
+        locator.scroll_into_view_if_needed()
+        locator.click()
+
+    def enter_text(self, locator: Locator, value: str):
+        print(f"Enter Text {locator}")
+        self.focus_element(locator)
+        locator.fill(value)
+        expect(locator).to_have_value(re.compile(r".+"))
+        self.page.keyboard.press("Enter", delay=2500)
+
     # Assertions
 
     def verify_text_visible(self, text):
         print(f"Verifying text visible: '{text}'")
-        expect(self.lbl_text(text)).to_be_visible()
-        assert(self.lbl_text(text).is_visible())
+        locator = self.lbl_text(text)
+        expect(locator).to_be_visible()
+        assert locator.is_visible()
+
+    def verify_element_visible(self, locator: Locator):
+        print(f"Verifying element visible: '{locator}'")
+        expect(locator).to_be_visible()
+        assert locator.is_visible()
+
+    def verify_element_not_visible(self, locator: Locator):
+        print(f"Verifying element not visible: '{locator}'")
+        expect(locator).not_to_be_visible()
+        assert locator.is_hidden()
+
+    def verify_element_attribute(self, locator: Locator, attr_name: str, expected_attr_value: str):
+        print(f"Verifying element attribute: '{attr_name}' ({expected_attr_value})")
+        get_value = locator.get_attribute(attr_name)
+        print(f"Getting attribute: '{attr_name}' ({expected_attr_value})")
+        assert expected_attr_value in get_value
--- tests/test_interactions.py
+++ tests/test_interactions.py
@@ -1,8 +1,9 @@
-from code import interact
+from datetime import datetime
+from time import sleep
 
 import pytest
 from dotenv import load_dotenv
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 
 from src.pages.interactions_page import InteractionsPage
 
@@ -20,22 +21,117 @@ class TestInteraction:
 
         print("Test complete, performing cleanup...")
 
-
-
     @pytest.mark.interactions
     @pytest.mark.TC1
-    def test_sortable_interactions(self, playwright_page):
-
+    def test_sortable_interactions(self):
         self.inter_page.verify_text_visible("Sortable")
 
     @pytest.mark.interactions
     @pytest.mark.TC2
-    def test_resizeable_interactions(self, playwright_page):
+    def test_resizeable_interactions(self):
         self.inter_page.verify_text_visible("Sortable")
 
     @pytest.mark.interactions
     @pytest.mark.TC3
-    def test_droppable_interactions(self, playwright_page: Page):
-        self.inter_page.navigate_to("Interactions")
+    def test_droppable_interactions(self):
         self.inter_page.verify_text_visible("Sortable")
         self.inter_page.drag_and_drop()
+
+    @pytest.mark.interactions
+    @pytest.mark.TC8
+    def test_accordion_elements_interactions(self):
+        print("Testing Accordion elements interactions...")
+
+        self.inter_page.verify_text_visible("Accordion")
+
+        self.inter_page.verify_element_visible(self.inter_page.lbl_text("Accordion"))
+
+        print(f"Verify Default Element visible for Accordion")
+
+        for txt in ["Section 1", "Section 2", "Content 1"]:
+            self.inter_page.verify_element_visible(self.inter_page.lbl_text(txt))
+
+        self.inter_page.verify_element_not_visible(self.inter_page.lbl_text("Content 2"))
+
+        self.inter_page.click_element(self.inter_page.lbl_text("Section 2"))
+
+        self.inter_page.verify_element_attribute(
+            self.inter_page.lbl_text("Section 1"),
+            attr_name="aria-expanded",
+            expected_attr_value="false"
+        )
+        self.inter_page.verify_element_not_visible(self.inter_page.lbl_text("Content 1"))
+
+    @pytest.mark.interactions
+    @pytest.mark.TC9
+    @pytest.mark.parametrize("element_test, autocomplete_value", [
+        pytest.param("Autocomplete", "Dheo Claveria Testing", id="1"),
+        pytest.param("Autocomplete", "Bliss Coders Assignment", id="2")
+    ])
+    def test_auto_complete_elements_interactions(self, element_test, autocomplete_value):
+        print("Testing Autocomplete elements interactions...")
+
+        self.inter_page.verify_text_visible(element_test)
+
+        self.inter_page.verify_element_visible(self.inter_page.lbl_text(element_test))
+
+        self.inter_page.enter_text(self.inter_page.txt_autocomplete(), autocomplete_value)
+        sleep(3)
+
+        self.inter_page.verify_element_attribute(
+            self.inter_page.txt_autocomplete(),
+            attr_name="autocomplete",
+            expected_attr_value="off"
+        )
+
+    @pytest.mark.interactions
+    @pytest.mark.TC10
+    def test_date_picker_elements_interactions(self):
+        print("Testing Date picker elements interactions...")
+
+        self.inter_page.verify_text_visible("Datepicker")
+
+        self.inter_page.verify_element_visible(self.inter_page.lbl_text("Datepicker"))
+
+        date_today = datetime.now().strftime("%m/%d/%Y")
+        self.inter_page.enter_text(self.inter_page.txt_date_picker(), date_today)
+        sleep(3)
+
+    @pytest.mark.interactions
+    @pytest.mark.TC11
+    @pytest.mark.parametrize("element_test, progress_bar_value", [
+        pytest.param("Progressbar", "60", id="1")])
+    def test_progress_bar_elements_interactions(self, element_test, progress_bar_value):
+        print("Testing Progress bar elements interactions...")
+
+        self.inter_page.verify_text_visible(element_test)
+
+        self.inter_page.verify_element_visible(self.inter_page.lbl_text(element_test))
+
+        self.inter_page.verify_element_attribute(
+            self.inter_page.get_locator_by(locator_type="role", value="progressbar"),
+            attr_name="aria-valuenow",
+            expected_attr_value=progress_bar_value)
+
+    @pytest.mark.interactions
+    @pytest.mark.TC12
+    def test_tabs_elements_interactions(self):
+        print("Testing Tabs elements interactions...")
+
+        self.inter_page.verify_text_visible("Tabs")
+
+        self.inter_page.verify_element_visible(self.inter_page.lbl_text("Tabs"))
+        self.inter_page.verify_element_visible(self.inter_page.tab_selected("1"))
+        self.inter_page.verify_element_visible(self.inter_page.lbl_text("Tab 1 content"))
+        self.inter_page.verify_element_not_visible(self.inter_page.lbl_text("Tab 2 content"))
+
+        self.inter_page.click_element(self.inter_page.tab_selected("2"))
+
+        self.inter_page.verify_element_attribute(
+            self.inter_page.tab_selected("1"),
+            attr_name="aria-expanded",
+            expected_attr_value="false"
+        )
+        self.inter_page.verify_element_not_visible(self.inter_page.lbl_text("Tab 1 content"))
+        sleep(1.5)
+        self.inter_page.verify_element_visible(self.inter_page.tab_selected("1"))
